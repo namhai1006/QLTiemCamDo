d41a885ae32f6d0fa5c9c98bc53cc1e1e205839a
Quoc Dat <quocdat@example.com>
2024-11-01 21:24:44 +0700

    fix delete pawnItem

--- src/routes/pawnItemRoutes.js
+++ src/routes/pawnItemRoutes.js
@@ -11,6 +11,6 @@ const Router = express.Router();
 Router.route("/get-all-pawn-item").get(verifyAccessToken, getAllPawnItems);
 Router.route("/create-pawn-item").post(verifyAccessToken, createPawnItem);
 Router.route("/get-pawn-item-by-id/:id").post(verifyAccessToken, getPawnItemsById);
-Router.route("/delete-pawn-item").post(verifyAccessToken, deletePawnItem);
+Router.route("/delete-pawn-item/:id").post(verifyAccessToken, deletePawnItem);
 
 export default Router;
