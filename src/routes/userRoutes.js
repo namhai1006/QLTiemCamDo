8341c1badf75d6bdd018075b744c67bc0b9817bd
Quoc Dat <quocdat@example.com>
2024-11-04 20:57:49 +0700

    update profile and change password

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,11 +2,15 @@ import express from "express";
 import {
   getAllPendingUser,
   getAllUsers,
+  updateProfile,
+  changePassword,
 } from "../controllers/userController.js";
 import { verifyAccessToken } from "../middlewares/verifyAccess.js";
 
 const Router = express.Router();
 Router.route("/get-all-user").get(verifyAccessToken, getAllUsers);
 Router.route("/get-all-pending-user").get(verifyAccessToken, getAllPendingUser);
+Router.route("/update-profile").put(verifyAccessToken, updateProfile);
+Router.route("/change-password").put(verifyAccessToken, changePassword);
 
 export default Router;
